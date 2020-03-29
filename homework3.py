f9b585863561538985085f2cf52dc35b951b1651
junggghee <61975196+junggghee@users.noreply.github.com>
2020-03-29 14:13:23 +0900

    homework_3week_junghee

--- homework3.py
+++ homework3.py
@@ -1,12 +1,27 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get(
-    "https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309",headers=headers
-)
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309', headers=headers)
 
-print(data)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+#순위 / 곡 제목 / 가수 (네이버영화 실습과 동일하게 진행)
+
+song_list = soup.select('table.list-wrap>tbody>tr')
+rank = 0
+for song in song_list:
+    title = song.select('a.title.ellipsis')
+    singer = song.select('a.artist.ellipsis')
+    rank += 1
+    title_el = title[0].text
+    singer_el = singer[0].text
+    print(rank, title_el.strip(), singer_el)
+
+
+
+
+
 
 
 
--- practice.py
+++ practice.py
@@ -1,12 +1,20 @@
-a = ['사과','감','감','배','포도','포도','딸기','포도','감','수박','딸기']
+import requests
+from bs4 import BeautifulSoup
 
-def count_list(a_list):
-    r = {}
-    for f in a_list:
-        if r.get(f) == None:
-            r[f] = 1
-        else:
-            r[f] += 1
-    return r
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303', headers=headers)
 
-print(count_list(a))
+soup = BeautifulSoup(data.text, 'html.parser')
+
+movie_info = soup.select('table.list_ranking>tbody>tr')
+rank = 0
+for movie in movie_info:
+    title_el = movie.select('a')
+    point_el = movie.select('td.point')
+    if len(title_el) > 0:
+        rank += 1
+        title = title_el[0].text
+        point = point_el[0].text
+        print(rank, title, point)
+
